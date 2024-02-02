ce18e21ffdb3f6d0f8ddf24d9a013a6e1824b40a
Samuel Xavier <samuelxpp@gmail.com>
2024-02-02 20:22:57 -0300

    Cleanup - removing files

--- models.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import Column, Integer, String, Sequence
-from database import Base
-
-class Item(Base):
-    __tablename__ = 'items'
-
-    id = Column(Integer, Sequence("item_id_sequence"), primary_key=True)
-    task = Column(String)
--- schemas.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel
-
-class Item_pydantic(BaseModel):
-    task: str
-
-
